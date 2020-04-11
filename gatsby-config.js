1c92fc169f6771665777fec3e5dba5dce6fa2847
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2020-04-11 16:47:40 +0530

    updated blog posts

---
+++ gatsby-config.js
@@ -0,0 +1,82 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+module.exports = {
+  pathPrefix: "/blog",
+  siteMetadata: {
+    siteTitle: `Venu's Web Space`,
+    siteTitleAlt: `Venu's Web Space`,
+    siteHeadline: `Venu's Blog`,
+    siteUrl: `https://vchrombie.github.io/`,
+    siteDescription: `Venu's Blog.`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@vchrombie`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+          {
+            title: `Contact`,
+            slug: `/contact`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/lekoarts_de`,
+          },
+          {
+            name: `GitHub`,
+            url: `https://github.com/vchrombie`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+  ],
+}
